c64ef70426f18d6a8b1dedbe847ca356b84b755d
Lara Scremin <lscreminmendes@gmail.com>
2024-03-20 19:59:10 -0300

    gallery

--- src/paintings/Gallery.jsx
+++ src/paintings/Gallery.jsx
@@ -20,6 +20,18 @@ import PaintingSeventeen from "../assets/cathedral.png";
 import PaintingEighteen from "../assets/balbi.png";
 import PaintingNineteen from "../assets/cupid.png";
 import PaintingTwenty from "../assets/eldest.png";
+import PaintingTOne from "../assets/agnus.png";
+import PaintingTTwo from "../assets/crucifixion.png";
+import PaintingTThree from "../assets/hercules.png";
+import PaintingTFour from "../assets/saint-francis.png";
+import PaintingTFive from "../assets/suzanna.png";
+import PaintingTSix from "../assets/magdalene.png";
+import PaintingTSeven from "../assets/slaying.png";
+import PaintingTEight from "../assets/lute-player.png";
+import PaintingTNine from "../assets/magi.png";
+import PaintingThirty from "../assets/still-life.png";
+import PaintingThOne from "../assets/denial.png";
+import PaintingThTwo from "../assets/patiente.png";
 
 function Picture() {
   return (
@@ -144,6 +156,78 @@ function Picture() {
         workName="Five Eldest Children of Charles I"
         workPainter="Anthony van Dyck"
         workYear={1637}
+      />
+      <Frame
+        image={PaintingTOne}
+        workName="Agnus Dei"
+        workPainter="Francisco de Zurbarán"
+        workYear={1640}
+      />
+      <Frame
+        image={PaintingTTwo}
+        workName="Saint Luke Painting the Crucifixion"
+        workPainter="Francisco de Zurbarán"
+        workYear={1650}
+      />
+      <Frame
+        image={PaintingTThree}
+        workName="Hercules and the Hydra"
+        workPainter="Francisco de Zurbarán"
+        workYear={1639}
+      />
+      <Frame
+        image={PaintingTFour}
+        workName="Saint Francis in Meditation"
+        workPainter="Francisco de Zurbarán"
+        workYear={1635}
+      />
+      <Frame
+        image={PaintingTFive}
+        workName="Susanna and the Elders"
+        workPainter="Artemisia Gentileschi"
+        workYear={1610}
+      />
+      <Frame
+        image={PaintingTSix}
+        workName="Mary Magdalene"
+        workPainter="Artemisia Gentileschi"
+        workYear={1618}
+      />
+      <Frame
+        image={PaintingTSeven}
+        workName="Judith Slaying Holofernes"
+        workPainter="Artemisia Gentileschi"
+        workYear={1613}
+      />
+      <Frame
+        image={PaintingTEight}
+        workName="Self-Portrait as a Lute Player"
+        workPainter="Artemisia Gentileschi"
+        workYear={1617}
+      />
+      <Frame
+        image={PaintingTNine}
+        workName="Adoration of the Magi"
+        workPainter="Artemisia Gentileschi"
+        workYear={1637}
+      />
+      <Frame
+        image={PaintingThirty}
+        workName="Adoration of the Magi"
+        workPainter="Artemisia Gentileschi"
+        workYear={1637}
+      />
+      <Frame
+        image={PaintingThOne}
+        workName="The Denial of Saint Peter"
+        workPainter="Artemisia Gentileschi"
+        workYear={1637}
+      />
+      <Frame
+        image={PaintingThTwo}
+        workName="Magdalene Patiente"
+        workPainter="Georges de La Tour"
+        workYear={1640}
       />
     </>
   );
